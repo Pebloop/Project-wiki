e83dc89f61c6d5bec4da757527e26f283a4cfabd
Pebloop <pebloop@outlook.com>
2025-03-11 06:00:57 +0100

    feat: add main menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,16 @@
 </script>
 
 {@render children()}
+
+<style>
+	:global(body) {
+		font-family: 'Roboto', sans-serif;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		:global(body) {
+			background-color: #1a202c;
+			color: #e2e8f0;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center h-screen">
+	<div>
+		<h1 class="text-2xl font-bold pb-12">My projects</h1>
+		<div class="flex flex-col space-y-4 ">
+			<a href="/goi" class="hover:font-bold" >Goi</a>
+			<a href="/cave-breaker" class="hover:font-bold" >Cave Breaker</a>
+			<a href="/wordly" class="hover:font-bold" >Wordly</a>
+			<a href="/gumicode" class="hover:font-bold" >Gumicode</a>
+		</div>
+	</div>
+</div>
